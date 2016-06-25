752560708f39844b29a56c2c6c1248d5d7d4bdc3
zip1982b <zip1982b@gmail.com>
2016-06-25 02:39:05 +0200

    home pc 23.06.16

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
 
 
 appFlask = Flask(__name__)  # создаём объект приложения (appFlask это экземпляр класса Flask)
 appFlask.config.from_object('config')
+db = SQLAlchemy(appFlask)
 
 
-from app import views  # импортируем модуль представлений - это обработчики, которые отвечают на запросы веб-браузера
-                       # (из папки app импортируем модуль views.py)
-                       # Почему в конце импорт? чтобы избежать циклических ссылок
+
+from app import views, models   # импортируем модуль представлений - это обработчики, которые отвечают на запросы веб-браузера
+                                # (из папки app импортируем модуль views.py)
+                                # Почему в конце импорт? чтобы избежать циклических ссылок
+                                # (из папки app импортируем модуль models.py)
--- config.py
+++ config.py
@@ -1,5 +1,14 @@
+# -*- coding: utf-8 -*-
+import os
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')  # Это путь к файлу с нашей базой данных
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')  # это папка, где мы будем хранить файлы SQLAlchemy-migrate
 
 
 
