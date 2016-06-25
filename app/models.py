3bbc67dde4905d9fcd9ec423dc33e6ebd27cd2c0
zip1982b <zip1982b@gmail.com>
2016-06-25 15:14:11 +0200

    home pc 23.06.16

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
 from app import db
 
+""" class User это название таблицы в базе app.db
+    id, nickname, email - это наши записи в таблице user (необходимо добавить password)"""
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
